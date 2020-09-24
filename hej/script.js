7996dc66afa1c2df2222234f94b235563def9c56
JenkkiP <71699332+JenkkiP@users.noreply.github.com>
2020-09-24 09:00:37 +0300

    efe

--- hej/script.js
+++ hej/script.js
@@ -1,9 +1,18 @@
 let row = document.getElementsByClassName("ok");
-
-
+let i = 0;
+let a = ["lightgray"]
+let b = 0;
 changeColor();
 
 function changeColor()
 {
+    console.log("hejjjj")
+    document.body.style.backgroundColor = a[b];
+    b++
+}
 
+while(i < row.length)
+{
+    row[i].addEventListener("click", changeColor);
+    i++
 }
